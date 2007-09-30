1db4cfe39d941755eefaa42fefb136d91ea2fd3d
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-09-30 16:50:12 -0500

    dwhittle: merge trunk changes to branch (revs: 3306:3320)
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle/lib/plugins/sfPropelPlugin@5321 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/functional/fixtures/config/config.php
+++ test/functional/fixtures/config/config.php
@@ -2,8 +2,5 @@
 
 // symfony directories
 
-$sf_symfony_root_dir = dirname(__FILE__).'/../../../../../../..';
-
-$sf_symfony_lib_dir  = $sf_symfony_root_dir.DIRECTORY_SEPARATOR.'lib';
-$sf_symfony_data_dir = $sf_symfony_root_dir.DIRECTORY_SEPARATOR.'data';
-
+$sf_symfony_lib_dir  = realpath(dirname(__FILE__).'/../../../../../../');
+$sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../../../../../../data');
