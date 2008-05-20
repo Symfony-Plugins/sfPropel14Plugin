672eff0d0474aaea75c358b3f187674d185ad7cd
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-05-20 18:18:21 -0500

    sfPropelPlugin: fixed include_paths to use real absolute path
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/branches/1.3@9122 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/propel/addon/sfPropelAutoload.php
+++ lib/propel/addon/sfPropelAutoload.php
@@ -17,8 +17,8 @@
  * @version    SVN: $Id$
  */
 
-require_once('propel/Propel.php');
+set_include_path(get_include_path().PATH_SEPARATOR.realpath(dirname(__FILE__).'/../../vendor/').'/');
 
-set_include_path(get_include_path().PATH_SEPARATOR.sfConfig::get('sf_symfony_lib_dir').PATH_SEPARATOR.dirname(__FILE__).'/../lib/vendor/');
+require_once('propel/Propel.php');
 
 sfPropel::initialize(sfProjectConfiguration::getActive()->getEventDispatcher());
