f4ca1c7525da36a90f2772e60199ddbd4ddd62c6
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-06-15 17:38:09 -0500

    sfPropelPlugin: fixed stupid typo
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/branches/1.3@9593 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/sfPropelGraphvizTask.class.php
+++ lib/task/sfPropelGraphvizTask.class.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-require_once(dirname(FILE).'/sfPropelBaseTask.class.php'); 
+require_once(dirname(__FILE__).'/sfPropelBaseTask.class.php');
 
 /**
  * Generates a graphviz chart of current object model.
