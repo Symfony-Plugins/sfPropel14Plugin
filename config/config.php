09ade9483bbeb763e087dc230c43356cc8338620
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-06-22 22:26:56 -0500

    sfPropelPlugin: fixed include path for behaviors (closes #3772)
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/branches/1.3@9766 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -1,3 +1,3 @@
 <?php
 
-set_include_path(get_include_path().PATH_SEPARATOR.sfConfig::get('sf_symfony_lib_dir').PATH_SEPARATOR.realpath(dirname(__FILE__).'/../lib/vendor/').'/');
+set_include_path(get_include_path().PATH_SEPARATOR.sfConfig::get('sf_root_dir').PATH_SEPARATOR.sfConfig::get('sf_symfony_lib_dir').PATH_SEPARATOR.realpath(dirname(__FILE__).'/../lib/vendor/').'/');
