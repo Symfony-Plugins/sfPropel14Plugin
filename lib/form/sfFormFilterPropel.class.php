84031c5adf9383523db1bb88cc0c946872131aaf
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-19 10:21:38 -0500

    [1.2] added the new filtering system based on the new form framework
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/trunk@11675 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/form/sfFormFilterPropel.class.php
@@ -0,0 +1,203 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfFormFilterPropel is the base class for filter forms based on Propel objects.
+ *
+ * @package    symfony
+ * @subpackage form
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+abstract class sfFormFilterPropel extends sfFormFilter
+{
+  /**
+   * Returns the current model name.
+   */
+  abstract public function getModelName();
+
+  public function getCriteria()
+  {
+    if (!$this->isValid())
+    {
+      throw $this->getErrorSchema();
+    }
+
+    return $this->buildCriteria($this->processValues());
+  }
+
+  /**
+   * Processes cleaned up values with user defined methods.
+   *
+   * To process a value before it is used by the buildCriteria() method,
+   * you need to define an convertXXXValue() method where XXX is the PHP name
+   * of the column.
+   *
+   * The method must return the processed value or false to remove the value
+   * from the array of cleaned up values.
+   *
+   * @return array An array of cleaned up values processed by the user defined methods
+   */
+  public function processValues()
+  {
+    // see if the user has overridden some column setter
+    $values = $this->values;
+    foreach ($this->values as $field => $value)
+    {
+      try
+      {
+        $method = sprintf('convert%sValue', call_user_func(array(constant($this->getModelName().'::PEER'), 'translateFieldName'), $field, BasePeer::TYPE_FIELDNAME, BasePeer::TYPE_PHPNAME));
+      }
+      catch (Exception $e)
+      {
+        // no a "real" column of this object
+        continue;
+      }
+
+      if (method_exists($this, $method))
+      {
+        if (false === $ret = $this->$method($value))
+        {
+          unset($values[$field]);
+        }
+        else
+        {
+          $values[$field] = $ret;
+        }
+      }
+    }
+
+    return $values;
+  }
+
+  public function buildCriteria(array $values)
+  {
+    $criteria = new Criteria();
+
+    $peer = constant($this->getModelName().'::PEER');
+    foreach ($this->getFields() as $field => $type)
+    {
+      if (!isset($values[$field]) || is_null($values[$field]) || '' == $values[$field])
+      {
+        continue;
+      }
+
+      try
+      {
+        $method = sprintf('add%sColumnCriteria', call_user_func(array($peer, 'translateFieldName'), $field, BasePeer::TYPE_FIELDNAME, BasePeer::TYPE_PHPNAME));
+      }
+      catch (Exception $e)
+      {
+        // not a "real" column
+        if (!method_exists($this, $method = sprintf('add%sColumnCriteria', sfInflector::camelize($field))))
+        {
+          throw new LogicException(sprintf('You must define a "%s" method to be able to filter with the "%s" field.', $method, $field));
+        }
+      }
+
+      if (method_exists($this, $method))
+      {
+        $this->$method($criteria, $field, $values[$field]);
+      }
+      else
+      {
+        if (!method_exists($this, $method = sprintf('add%sCriteria', $type)))
+        {
+          throw new LogicException(sprintf('Unable to filter for the "%s" type.', $type));
+        }
+
+        $this->$method($criteria, $field, $values[$field]);
+      }
+    }
+
+    return $criteria;
+  }
+
+  protected function addForeignKeyCriteria(Criteria $criteria, $field, $value)
+  {
+    $colname = $this->getColname($field);
+
+    if (is_array($value))
+    {
+      $values = $value;
+      $value = array_pop($values);
+      $criterion = $criteria->getNewCriterion($colname, $value);
+
+      foreach ($values as $value)
+      {
+        $criterion->addOr($criteria->getNewCriterion($colname, $value));
+      }
+
+      $criteria->add($criterion);
+    }
+    else
+    {
+      $criteria->add($colname, $value);
+    }
+  }
+
+  protected function addTextCriteria(Criteria $criteria, $field, $values)
+  {
+    $colname = $this->getColname($field);
+
+    if (isset($values['is_empty']) && $values['is_empty'])
+    {
+      $criterion = $criteria->getNewCriterion($colname, '');
+      $criterion->addOr($criteria->getNewCriterion($colname, null, Criteria::ISNULL));
+      $criteria->add($criterion);
+    }
+    else if (isset($values['text']) && '' != $values['text'])
+    {
+      $criteria->add($colname, '%'.$values['text'].'%', Criteria::LIKE);
+    }
+  }
+
+  protected function addBooleanCriteria(Criteria $criteria, $field, $value)
+  {
+    $criteria->add($this->getColname($field), $value);
+  }
+
+  protected function addDateCriteria(Criteria $criteria, $field, $values)
+  {
+    $colname = $this->getColname($field);
+
+    if (isset($values['is_empty']) && $values['is_empty'])
+    {
+      $criteria->add($colname, null, Criteria::ISNULL);
+    }
+    else
+    {
+      $criterion = null;
+      if (!is_null($values['from']) && !is_null($values['to']))
+      {
+        $criterion = $criteria->getNewCriterion($colname, $values['from'], Criteria::GREATER_EQUAL);
+        $criterion->addAnd($criteria->getNewCriterion($colname, $values['to'], Criteria::LESS_EQUAL));
+      }
+      else if (!is_null($values['from']))
+      {
+        $criterion = $criteria->getNewCriterion($colname, $values['from'], Criteria::GREATER_EQUAL);
+      }
+      else if (!is_null($values['to']))
+      {
+        $criterion = $criteria->getNewCriterion($colname, $values['to'], Criteria::LESS_EQUAL);
+      }
+
+      if (!is_null($criterion))
+      {
+        $criteria->add($criterion);
+      }
+    }
+  }
+
+  protected function getColName($field)
+  {
+    return call_user_func(array(constant($this->getModelName().'::PEER'), 'translateFieldName'), $field, BasePeer::TYPE_FIELDNAME, BasePeer::TYPE_COLNAME);
+  }
+}
