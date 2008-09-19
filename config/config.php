305a9f0b2e7052fc9ccf2bea1eeb61967c2964e1
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-19 00:33:16 -0500

    [1.2] add sf_orm setting in sfPropelPlugin
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/trunk@11666 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -1,13 +1,12 @@
 <?php
 
-if (sfConfig::get('sf_orm') == 'propel')
+sfConfig::get('sf_orm', 'propel');
+
+set_include_path(sfConfig::get('sf_root_dir').PATH_SEPARATOR.sfConfig::get('sf_symfony_lib_dir').PATH_SEPARATOR.realpath(dirname(__FILE__).'/../lib/vendor/').'/'.PATH_SEPARATOR.get_include_path());
+
+if (sfConfig::get('sf_web_debug'))
 {
-  set_include_path(sfConfig::get('sf_root_dir').PATH_SEPARATOR.sfConfig::get('sf_symfony_lib_dir').PATH_SEPARATOR.realpath(dirname(__FILE__).'/../lib/vendor/').'/'.PATH_SEPARATOR.get_include_path());
+  require_once dirname(__FILE__).'/../lib/debug/sfWebDebugPanelPropel.class.php';
 
-  if (sfConfig::get('sf_web_debug'))
-  {
-    require_once dirname(__FILE__).'/../lib/debug/sfWebDebugPanelPropel.class.php';
-
-    $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelPropel', 'listenToAddPanelEvent'));
-  }
+  $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelPropel', 'listenToAddPanelEvent'));
 }
