cffbed9e56758bac7a5b5e75a49e5120349cf8ee
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-10-13 09:55:59 -0500

    [1.2] Fixed typo
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/trunk@12167 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-sfConfig::get('sf_orm', 'propel');
+sfConfig::set('sf_orm', 'propel');
 
 set_include_path(sfConfig::get('sf_root_dir').PATH_SEPARATOR.sfConfig::get('sf_symfony_lib_dir').PATH_SEPARATOR.realpath(dirname(__FILE__).'/../lib/vendor/').'/'.PATH_SEPARATOR.get_include_path());
 
