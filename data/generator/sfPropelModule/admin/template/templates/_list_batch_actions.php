54508ad501f02ec1820262b1b3b30296ba2f7b76
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-10-31 15:46:33 -0500

    [1.2] fixed typo
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/trunk@12511 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/admin/template/templates/_list_batch_actions.php
+++ data/generator/sfPropelModule/admin/template/templates/_list_batch_actions.php
@@ -1,7 +1,7 @@
 <?php if ($listActions = $this->configuration->getValue('list.batch_actions')): ?>
   <li class="sf_admin_batch_actions_choice">
     <select name="batch_action">
-      <option>[?php echo __('Choose an action') ?]</option>
+      <option value="">[?php echo __('Choose an action') ?]</option>
 <?php foreach ((array) $listActions as $action => $params): ?>
 <?php echo $this->addCredentialCondition('<option value="'.$action.'">'.$params['label'].'</option>', $params) ?>
 <?php endforeach; ?>
