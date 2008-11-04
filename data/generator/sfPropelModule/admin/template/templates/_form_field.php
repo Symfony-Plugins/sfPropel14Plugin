e839fb79bf2fb9dc7bfa99dcac728c231dbde38d
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-11-04 12:15:18 -0600

    [1.2] made some more tweaks for people with output escaping set to off (closes #4792)
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/trunk@12640 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/admin/template/templates/_form_field.php
+++ data/generator/sfPropelModule/admin/template/templates/_form_field.php
@@ -1,7 +1,7 @@
 [?php if ($field->isPartial()): ?]
-  [?php include_partial('<?php echo $this->getModuleName() ?>/'.$name, array('form' => $form, 'attributes' => $attributes->getRawValue())) ?]
+  [?php include_partial('<?php echo $this->getModuleName() ?>/'.$name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
 [?php elseif ($field->isComponent()): ?]
-  [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('form' => $form, 'attributes' => $attributes->getRawValue())) ?]
+  [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
 [?php else: ?]
   <div class="[?php echo $class ?][?php $form[$name]->hasError() and print ' errors' ?]">
     [?php echo $form[$name]->renderError() ?]
