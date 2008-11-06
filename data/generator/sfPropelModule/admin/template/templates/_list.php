7781b2cc9229d2af11640426d2b075fe92470f82
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-11-06 05:53:28 -0600

    [1.2] added i18n missing catalogue information (closes #4841 - patch from Pascal)
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/trunk@12683 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/admin/template/templates/_list.php
+++ data/generator/sfPropelModule/admin/template/templates/_list.php
@@ -21,7 +21,7 @@
               [?php include_partial('<?php echo $this->getModuleName() ?>/pagination', array('pager' => $pager)) ?]
             [?php endif; ?]
 
-            [?php echo format_number_choice('[0] no result|[1] 1 result|(1,+Inf] %1% results', array('%1%' => $pager->getNbResults()), $pager->getNbResults()) ?]
+            [?php echo format_number_choice('[0] no result|[1] 1 result|(1,+Inf] %1% results', array('%1%' => $pager->getNbResults()), $pager->getNbResults(), 'sf_admin') ?]
             [?php if ($pager->haveToPaginate()): ?]
               [?php echo __('(page %%page%%/%%nb_pages%%)', array('%%page%%' => $pager->getPage(), '%%nb_pages%%' => $pager->getLastPage()), 'sf_admin') ?]
             [?php endif; ?]
