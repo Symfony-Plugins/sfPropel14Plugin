ffeeddf21389bb62e1b7336eff070187ffd08cc1
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-11-17 15:43:31 -0600

    [1.2] sfPropelPlugin: fixes issue with admin generator and multi-part file forms (closes #4946)
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlugin/trunk@13084 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/admin/parts/processFormAction.php
+++ data/generator/sfPropelModule/admin/parts/processFormAction.php
@@ -1,6 +1,6 @@
   protected function processForm(sfWebRequest $request, sfForm $form)
   {
-    $form->bind($request->getParameter($form->getName()));
+    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
     if ($form->isValid())
     {
       $this->getUser()->setFlash('notice', $form->getObject()->isNew() ? 'The item was created successfully.' : 'The item was updated successfully.');
